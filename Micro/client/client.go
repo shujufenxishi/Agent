bcea23ef23186dbe03cc26c8c9f60d0667e8310a
shujufenxishi <52852532+shujufenxishi@users.noreply.github.com>
2021-01-06 22:05:16 +0800

    Add files via upload

---
+++ Micro/client/client.go
@@ -0,0 +1,25 @@
+package main
+ 
+import (
+ "context"
+ "fmt"
+ "github.com/micro/go-micro/v2"
+ "Micro/proto"
+)
+ 
+func main() {
+ 
+ // 创建一个服务
+ service := micro.NewService(micro.Name("greeter.client"))
+ // 初始化
+ service.Init()
+ // 创建一个微服务的客户端
+ greeter := pb.NewGreeterService("greeter", service.Client())
+ // 调用微服务
+ rsp, err := greeter.Hello(context.TODO(), &pb.Request{Name: "Hello Micro"})
+ if err != nil {
+  fmt.Println(err)
+ }
+ 
+ fmt.Println(rsp.Msg)
+}
---
+++ Micro/go.mod
@@ -0,0 +1,11 @@
+module Micro
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.3
+	github.com/micro/go-micro/v2 v2.9.1
+	google.golang.org/protobuf v1.25.0
+)
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.25.1
